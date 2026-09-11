fb937b953c6e4b035e6bcf51ac1ef7ec0c5f299a
amity321 <40298926+amity321@users.noreply.github.com>
2026-09-11 11:53:13 +0530

    app.py

--- app.py
+++ app.py
@@ -16,38 +16,38 @@ AREA_CONFIGS = {
     },
     "Area 04/05": {
         "title": "Area 04/05 Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS-YNMY8GAWDYkYoC2zW3riA8rnFhnP2hbFRisXYXlLb3Iv95jXyZEHjPUQsfFI4dFt_Z51N0932jPO/pub?gid=345050306&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS-YNMY8GAWDYkYoC2zW3riA8rnFhnP2hbFRisXYXlLb3Iv95jXyZEHjPUQsfFI4dFt_Z51N0932jPO/pub?gid=593280436&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZopDMRgkBThhmBF8NAXoBERx24tj7Ae2y6HlvimEHUhahXEWY8tmXoNDSM_MNlkDB7TfGpHB9I2H_/pub?gid=91556546&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZopDMRgkBThhmBF8NAXoBERx24tj7Ae2y6HlvimEHUhahXEWY8tmXoNDSM_MNlkDB7TfGpHB9I2H_/pub?gid=1951924870&single=true&output=csv"
     },
     "Area 06/07": {
         "title": "Area 06/07 Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7xjMzOaBojfDgk4JLijhGFxA987o2zi7-etXZ7RjYCS8WgZR0IU_gx1eyn5PIZc0HFCuUxwr9xY1/pub?gid=2114506451&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7xjMzOaBojfDgk4JLijhGFxA987o2zi7-etXZ7RjYCS8WgZR0IU_gx1eyn5PIZc0HFCuUxwr9xY1/pub?gid=687360060&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vStPdBa-nm7i9eHjSxpyrIOyyu5VJZo77E4KF3tk2R9ewp0hK58RDVYBKiW5UsRD2DxBTrafX-CfJry/pub?gid=1197458087&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vStPdBa-nm7i9eHjSxpyrIOyyu5VJZo77E4KF3tk2R9ewp0hK58RDVYBKiW5UsRD2DxBTrafX-CfJry/pub?gid=1371227319&single=true&output=csv"
     },
     "Area 08": {
         "title": "Area 08 Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSEnzn9n4L_uGO9VkLMe8_ylvyaZkskIZZEFJSTqXDQJJ1uEHevl9FfKWhnpcltGsDlhwsxnIEOflaK/pub?gid=1609301093&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSEnzn9n4L_uGO9VkLMe8_ylvyaZkskIZZEFJSTqXDQJJ1uEHevl9FfKWhnpcltGsDlhwsxnIEOflaK/pub?gid=1399282472&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6zlQsPz48l4GzLuFf76kSkQje3Y_eY_Y-qwIG9x4ehkPX76qr7f9_ScttwV3gJJrSX7B9xHO6Tfqp/pub?gid=1605324050&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6zlQsPz48l4GzLuFf76kSkQje3Y_eY_Y-qwIG9x4ehkPX76qr7f9_ScttwV3gJJrSX7B9xHO6Tfqp/pub?gid=1898823621&single=true&output=csv"
     },
     "Area 09/10": {
         "title": "Area 09/10 Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxzP4Xek0tErBRzBaaNpYvQfjCiCEbcvi3EwQU-PzUkr7uH319JvwGO6baTWnVqBcACiyKrz96D3s0/pub?gid=1452315016&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxzP4Xek0tErBRzBaaNpYvQfjCiCEbcvi3EwQU-PzUkr7uH319JvwGO6baTWnVqBcACiyKrz96D3s0/pub?gid=230600100&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7NvVXAcew2ZWcA_kSTmCQJk6OVq3RQfqGqCZ08jGKosNmTYWprvR4JUMC3-vXI28wF6HJ1B_Wk1uo/pub?gid=923164289&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7NvVXAcew2ZWcA_kSTmCQJk6OVq3RQfqGqCZ08jGKosNmTYWprvR4JUMC3-vXI28wF6HJ1B_Wk1uo/pub?gid=1187023151&single=true&output=csv"
     },
     "SPP TG": {
         "title": "SPP TG Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSv2eRmgJQbwKowu9TW_-4t-PiHzOZf3ENGr4Evum-vdB5qjSmTibk6GZOH5eHdibLRtF_6CQmjUZM-/pub?gid=1122212956&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSv2eRmgJQbwKowu9TW_-4t-PiHzOZf3ENGr4Evum-vdB5qjSmTibk6GZOH5eHdibLRtF_6CQmjUZM-/pub?gid=909280818&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPmgZl9jEQaGMQbxeOu0Xr_GtQ2P4_twAx2qNxUOjoYSvSW27vJsUgRtQB7XtIcU-bcCulPJLX3PLA/pub?gid=224363124&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPmgZl9jEQaGMQbxeOu0Xr_GtQ2P4_twAx2qNxUOjoYSvSW27vJsUgRtQB7XtIcU-bcCulPJLX3PLA/pub?gid=900388666&single=true&output=csv"
     },
     "SPP Boiler": {
         "title": "SPP Boiler Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1ml_IQGF1JYovetx58rU5XXPEiNmAuj11Vco1l9eO0DBCS_fWeZ-RAkFauvkDHlo2lMdYB1mLZJOa/pub?gid=53758522&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1ml_IQGF1JYovetx58rU5XXPEiNmAuj11Vco1l9eO0DBCS_fWeZ-RAkFauvkDHlo2lMdYB1mLZJOa/pub?gid=1357503643&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQVTH56rybWjsWYThgCiTWzafjabniWhqHUUuXoVdqexuWIjrmvh65AtimfDlFNB5V4StSi5G4BWuKf/pub?gid=1149044523&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQVTH56rybWjsWYThgCiTWzafjabniWhqHUUuXoVdqexuWIjrmvh65AtimfDlFNB5V4StSi5G4BWuKf/pub?gid=223018013&single=true&output=csv"
     },
     "C&I Sub Store": {
         "title": "C&I Sub Store Instrumentation Inventory",
-        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vShKNuZOaBiPF6I2VlTiA27izZ8DMIdRvOmo9sfVEwSz8wwoxw_f1rc4jFv4gOgMxw4kspnMEFxPTHi/pub?gid=1522866968&single=true&output=csv",
-        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vShKNuZOaBiPF6I2VlTiA27izZ8DMIdRvOmo9sfVEwSz8wwoxw_f1rc4jFv4gOgMxw4kspnMEFxPTHi/pub?gid=454657357&single=true&output=csv"
+        "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSbJUMrlU1bWLUsOt0tL-4xsBpsO2kt70Rq4am-OpMb7hsZZxe69JzLwBqT1EOLZtuU-PGkY-mx4EuZ/pub?gid=1168883154&single=true&output=csv",
+        "removal_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSbJUMrlU1bWLUsOt0tL-4xsBpsO2kt70Rq4am-OpMb7hsZZxe69JzLwBqT1EOLZtuU-PGkY-mx4EuZ/pub?gid=158170506&single=true&output=csv"
     }
 }
 
